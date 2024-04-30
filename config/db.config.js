1483d228a287e43bf188d4ec295a4bca93770c1b
Mohamed HAZAMI <meed.hazami@gmail.com>
2024-04-30 14:45:20 +0100

    fist commit

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "193.203.166.99",
+    USER: "u575385704_mascotteDB",
+    PASSWORD: `Y!e6HS$A!q`,
+    DB: "u575385704_mascotteDB",
+    dialect: "mysql",
+    pool: {
+    max: 10000,
+    min: 0,
+    acquire: 300000,
+    idle: 100000
+    }
+    };
